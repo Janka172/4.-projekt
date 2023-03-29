4b17dc9d74a5725bf77c2be7444b7a6b6cb47deb
Janka172 <jankahegedus872@gmail.com>
2023-03-29 12:27:18 +0200

    -ötlet +alap

---
+++ Projekt/Projekt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
